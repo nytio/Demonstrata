97be2903ae1eb3b231dc618e3bbb20dc1d22dffd
Mario Hernández M. <nytio@users.noreply.github.com>
2026-04-02 15:16:51 -0600

    feat: add modular arithmetic example theorem

--- Mimate/Basic.lean
+++ Mimate/Basic.lean
@@ -10,4 +10,7 @@ theorem two_add_two : (2 : Nat) + 2 = 4 := by
 theorem zmod_example : (2 : ZMod 5) + 3 = 0 := by
   decide
 
+theorem zmod_three_mul_five : (3 : ZMod 7) * 5 = 1 := by
+  decide
+
 end Mimate
